bfba84ad2e17402dfcfd42e0ff0a168f937a1f46
Tony Maldonado <21mtony@gmail.com>
2019-08-26 14:29:45 -0600

    Simple hello world java program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/* Antonio Maldonado
+ * CS 371
+ * August 26, 2019
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//prints Hello World! to the terminal window
+		System.out.println("Hello World!");
+	
+	}
+
+}
